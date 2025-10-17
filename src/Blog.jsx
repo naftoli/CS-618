5298ff80001d33be618b4dc9ac60d592cb936c9a
Naftoli Rapoport <naftolir@gmail.com>
2025-10-17 15:11:20 -0400

    feat: add header to blog

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -18,26 +18,29 @@ export function Blog() {
   const posts = postsQuery.data ?? []
 
   return (
-    <div style={{ padding: 8 }}>
-      <CreatePost />
-      <br />
-      <hr />
-      Filter by:
-      <PostFilter
-        field='author'
-        value={author}
-        onChange={(value) => setAuthor(value)}
-      />
-      <br />
-      <PostSorting
-        fields={['createdAt', 'updatedAt']}
-        value={sortBy}
-        onChange={(value) => setSortBy(value)}
-        orderValue={sortOrder}
-        onOrderChange={(orderValue) => setSortOrder(orderValue)}
-      />
-      <hr />
-      <PostList posts={posts} />
-    </div>
+    <>
+      <h1>My Blog</h1>
+      <div style={{ padding: 8 }}>
+        <CreatePost />
+        <br />
+        <hr />
+        Filter by:
+        <PostFilter
+          field='author'
+          value={author}
+          onChange={(value) => setAuthor(value)}
+        />
+        <br />
+        <PostSorting
+          fields={['createdAt', 'updatedAt']}
+          value={sortBy}
+          onChange={(value) => setSortBy(value)}
+          orderValue={sortOrder}
+          onOrderChange={(orderValue) => setSortOrder(orderValue)}
+        />
+        <hr />
+        <PostList posts={posts} />
+      </div>
+    </>
   )
 }
